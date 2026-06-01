11710dfc9acc23e364aa2237804ad76022817692
Ayoub Manar <Ayoub.Manar@jrvs.com>
2026-06-01 16:43:32 +0000

    Implement host_usage.sh scripts

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ]; then
-    echo "Illegal number of parameters"
-    exit 1
-fi
-
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
 
-hostname=$(hostname -f)
-memory_total=$(grep MemTotal /proc/meminfo | awk '{print $2}')
-cpu_model=$(lscpu | grep "Model name" | cut -d':' -f2 | xargs)
-cpu_number=$(lscpu | grep "^CPU(s):" | awk '{print $2}' | head -n1)
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
 
-insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_model, memory_total) VALUES ('$hostname', $cpu_number, '$cpu_model', $memory_total);"
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}' | tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1 | xargs)
+cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1 | xargs)
+disk_io=$(vmstat -d | awk '{print $10}' | tail -n1 | xargs)
+disk_available=$(df -BM / | tail -n1 | awk '{print $4}' | sed 's/M//')
+timestamp=$(vmstat -t | tail -n1 | awk '{print $18, $19}')
+
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')"
+
+insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', $host_id, $memory_free, $cpu_idle, $cpu_kernel, $disk_io, $disk_available);"
 
 export PGPASSWORD=$psql_password
 psql -h "$psql_host" -p "$psql_port" -d "$db_name" -U "$psql_user" -c "$insert_stmt"
-
 
 exit $?
